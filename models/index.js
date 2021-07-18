29594759f8e9612b9618119644251f2c14ad7c26
Amna Syeda <amna.syeda98@gmail.com>
2021-07-17 22:13:24 -0400

    fixing localhost password error

--- models/index.js
+++ models/index.js
@@ -5,10 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+})
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
