cfe149a0bfa4355bfa6ec5ede9641e7b27b8f4ff
Amna Syeda <amna.syeda98@gmail.com>
2021-07-18 13:44:27 -0400

    .env added to gitignore

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
